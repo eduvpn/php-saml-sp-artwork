cc29fbff24021ff5c17cc5f005bc2a6b7fc5f6ea
François Kooman <fkooman@tuxed.net>
2020-08-25 19:35:41 +0200

    remove bootstrap-reboot

--- views/base.php
+++ views/base.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$this->e($serviceName); ?></title>
-    <link href="<?=$this->getAssetUrl($requestRoot, 'css/bootstrap-reboot.min.css'); ?>" media="screen" rel="stylesheet">
     <link href="<?=$this->getAssetUrl($requestRoot, 'css/screen.css'); ?>" media="screen" rel="stylesheet">
     <link href="<?=$this->getAssetUrl($requestRoot, 'css/eduVPN/screen.css'); ?>" media="screen" rel="stylesheet">
     <script src="<?=$this->getAssetUrl($requestRoot, 'js/search.js'); ?>"></script>
